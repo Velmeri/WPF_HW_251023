ae480babbd023ab8fd67b16a2f8d1391600f3931
Daniel <Danielmikhh@gmail.com>
2024-01-09 06:00:46 +0100

    Добавьте файлы проекта.

---
+++ WPF_HW_251023/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_HW_251023
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Login_Click(object sender, RoutedEventArgs e)
+		{
+			string username = tbUsername.Text;
+			string password = tbPassword.Password;
+
+			if (username == "admin" && password == "password") {
+				MessageBox.Show("Login successful!", "Authentication", MessageBoxButton.OK, MessageBoxImage.Information);
+			} else {
+				MessageBox.Show("Login failed!", "Authentication", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void Cancel_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
